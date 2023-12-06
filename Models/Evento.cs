509e2c28fb0b8a5f185a0dc740d517061927b995
Andynajera <andrenaje94@hotmail.com>
2023-12-06 21:18:17 +0100

    models

---
+++ Models/Evento.cs
@@ -0,0 +1,17 @@
+
+
+
+//Clase
+using Classes;
+
+public class Evento{
+    
+//Propiedades
+    public int id { get; set; }
+    public string? Título {get;set;}
+    public string? Descripción {get;set;}
+    public int Precio {get;set;}
+    public DateTime  FechaReserva{get;set;}
+
+
+ }
